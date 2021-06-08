808ad85b0cff55edcbaedc73f3ba42f8205921d6
Matt <mpuent18@gmail.com>
2021-06-08 14:29:24 -0400

    Adding axios call to fetch data for row component

--- src/requests.js
+++ src/requests.js
@@ -1,7 +1,7 @@
 const API_KEY = "94fe7b34b355eb40584d5a16d89cc50f";
 
 const requests = {
-    fetchTrending: `/trending/all?api_key=${API_KEY}&language=en-US` ,
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US` ,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213` ,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US` ,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28` ,
