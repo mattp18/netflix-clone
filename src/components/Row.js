308d422979e7cdc8b4a5ab6768e11ffbd76c31d4
Matt <mpuent18@gmail.com>
2021-06-08 18:06:27 -0400

    Added movie map functionality and stylesheet for row

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,25 +1,49 @@
 import React, { useState, useEffect } from 'react'
 import axios from '../axios';
+import "../Row.css";
 
-function Row( { title, fetchUrl } ) {
+const base_url = "https://image.tmdb.org/t/p/original/"
+
+function Row( { title, fetchUrl, isLargeRow } ) {
     const [movies, setMovies] = useState([]);
 
     // A snippet of code which runs based on a speficic condition/variable
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
-            console.log(request);
+            setMovies(request.data.results);
             return request;
         }
         fetchData();
-    }, []);
+    }, [fetchUrl]); //required because we are using fetchUrl outside of the function. 
+
+    //mapping each movie image to an img tag
+    const mapMoveImage = 
+        movies.map(movie =>(
+            <img 
+            key={movie.id}
+            className= {`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
+                alt={movie.name}
+                />
+        ));
+    
+
+    console.log(movies);
 
     return (
-        <div>
+        <div className="row">
             <h2>{ title }</h2>
-          
 
-            {/** container -> posters */}
+            <div className="row__posters">
+                {/** several row__poster */}
+
+                {mapMoveImage}
+            </div>
+
+
+            
 
         </div>
     )
